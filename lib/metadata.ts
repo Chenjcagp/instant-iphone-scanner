16cfe2829c23e00df2bf689dee16314236deb322
Chen <evelfacker@gmail.com>
2026-01-29 23:28:02 +0800

    修改外链封面图为正确的网站首页

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -43,7 +43,7 @@ export async function constructMetadata({
       alt: pageTitle,
     }))
     : [{
-      url: `${siteConfig.url}/og.png`,
+      url: `${siteConfig.url}/opengraph-image.png`,
       alt: pageTitle,
     }]
 
